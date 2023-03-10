81428cd90308ddd3a0dffe56fbdf6614a42df327
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2023-03-10 15:42:04 +0900

    application.properties 및 기타 파일 수정
    application.properties 및 기타 파일 수정
     - hibernate format_sql 설정 (false -> true)

--- src/test/java/suso/backend/BackendApplicationTests.java
+++ src/test/java/suso/backend/BackendApplicationTests.java
@@ -2,16 +2,12 @@ package suso.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import suso.backend.domain.user.User;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
-		User user = new User();
-
 
 	}
-
 }
